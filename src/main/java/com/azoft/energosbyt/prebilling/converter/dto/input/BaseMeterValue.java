6468326c5bd0ba1cbe839dc1680863849d1d1e6d
krparen <ksackk@gmail.com>
2020-10-26 15:19:53 +0300

    setMv processor fixes

--- src/main/java/com/azoft/energosbyt/prebilling/converter/dto/input/BaseMeterValue.java
+++ src/main/java/com/azoft/energosbyt/prebilling/converter/dto/input/BaseMeterValue.java
@@ -1,10 +1,15 @@
 package com.azoft.energosbyt.prebilling.converter.dto.input;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 @Data
 public class BaseMeterValue {
+
+  private static final String dateFormat = "yyyy-MM-dd";
+
   String accountNumber;
   String branch;
   String meterNumber;
@@ -14,5 +19,6 @@ public class BaseMeterValue {
   String t3;
   String app_id;
   String channel;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = dateFormat) // отвечает за десериализацию
   Date mvDate;
 }
--- src/main/java/com/azoft/energosbyt/prebilling/converter/dto/output/Measure.java
+++ src/main/java/com/azoft/energosbyt/prebilling/converter/dto/output/Measure.java
@@ -3,10 +3,16 @@ package com.azoft.energosbyt.prebilling.converter.dto.output;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 public class Measure {
+
+  private static final String dateFormat = "yyyy-MM-dd";
+
   private String inform_system;
   private String account_number;
   private String contract_number;
@@ -18,6 +24,8 @@ public class Measure {
   private String ext_id_point_record;
   private String place;
   private String address_full;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = dateFormat) // отвечает за десериализацию
+  @DateTimeFormat(pattern = dateFormat) // отвечает за сериализацию
   private Date measure_date;
   private String measure_type;
   private String measure_form;
