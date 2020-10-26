778e00c8e787d06ad450a11c232feda1f02ca8c8
krparen <ksackk@gmail.com>
2020-10-26 14:56:50 +0300

    packaging to war

--- src/main/java/com/azoft/energosbyt/prebilling/converter/ConverterApplication.java
+++ src/main/java/com/azoft/energosbyt/prebilling/converter/ConverterApplication.java
@@ -2,9 +2,10 @@ package com.azoft.energosbyt.prebilling.converter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConverterApplication {
+public class ConverterApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(ConverterApplication.class, args);
