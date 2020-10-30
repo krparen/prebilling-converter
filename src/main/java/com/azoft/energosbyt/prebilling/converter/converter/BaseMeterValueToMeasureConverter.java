bedc1dad253d3e42a6aeadfbcaa98e2ba7774b1d
krparen <ksackk@gmail.com>
2020-10-30 08:34:41 +0300

    setMv registers fix

--- src/main/java/com/azoft/energosbyt/prebilling/converter/converter/BaseMeterValueToMeasureConverter.java
+++ src/main/java/com/azoft/energosbyt/prebilling/converter/converter/BaseMeterValueToMeasureConverter.java
@@ -19,40 +19,32 @@ public class BaseMeterValueToMeasureConverter implements Converter<BaseMeterValu
     output.setExt_id_meter(input.getMeterId());
     output.setMeter_number(input.getMeterNumber());
     output.setMeasure_date(input.getMvDate());
-    fillRegisters(input, output);
+    output.setRegister_value(getRegisterValues(input));
     output.setId_source(input.getApp_id());
     output.setId_channel(input.getChannel());
     return output;
   }
 
-  private void fillRegisters(BaseMeterValue input, Measure output) {
+  private List<RegisterValue> getRegisterValues(BaseMeterValue input) {
     List<RegisterValue> registerValues = new ArrayList<>();
-
-
-    String value = getValue(input);
-
-
-    RegisterValue registerValue = new RegisterValue();
-    registerValue.setValue(value);
-    registerValue.setExt_id_register(UUID.randomUUID().toString());
-
-    registerValues.add(registerValue);
-    output.setRegister_value(registerValues);
-  }
-
-  private String getValue(BaseMeterValue input) {
-    String value = "";
     if (Strings.isNotBlank(input.getT1())) {
-      value += input.getT1();
+      addRegisterValue(registerValues, input.getT1());
     }
 
     if (Strings.isNotBlank(input.getT2())) {
-      value += "," + input.getT2();
+      addRegisterValue(registerValues, input.getT2());
     }
 
     if (Strings.isNotBlank(input.getT3())) {
-      value += "," + input.getT3();
+      addRegisterValue(registerValues, input.getT3());
     }
-    return value;
+    return registerValues;
+  }
+
+  private void addRegisterValue(List<RegisterValue> registerValues, String value) {
+    RegisterValue registerValue = new RegisterValue();
+    registerValue.setValue(value);
+    registerValue.setExt_id(UUID.randomUUID().toString());
+    registerValues.add(registerValue);
   }
 }
